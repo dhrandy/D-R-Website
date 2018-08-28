8cf12ba597dd5fabb43a9ecec032b8a0b0e7be2c
dhrandy <dhrandy@gmail.com>
2018-08-27 22:01:27 -0400

    added scroll-reveal and change nav hover color

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.lead', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '200px',
+    mobile: false
+})
+
+sr.reveal('.reveal', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '50px',
+    mobile: false
+})
+
+sr.reveal('.container-two', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '50px',
+    mobile: false
+})
+
+sr.reveal('.container-three', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '50px',
+    mobile: false
+})
